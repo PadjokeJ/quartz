88e66fe9923a24f0fe33031870ddb7ae04773ef7
PadjokeJ <jonatan@padjokej.dev>
2026-05-08 20:29:03 +0200

    Quartz sync: May 8, 2026, 8:29 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -9,7 +9,7 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/PadjokeJ/quartz",
-      "Ba2 Repository": "https://github.com/PadjokeJ/ba2-EPFL"
+      "Ba2 Repository": "https://github.com/PadjokeJ/ba2-EPFL",
       "EPFL Page": "https://people.epfl.ch/jonatan.pfister"
     },
   }),
