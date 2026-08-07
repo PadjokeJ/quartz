469526b938967b5136438a2cf7b66eeb3e38a609
PadjokeJ <jonatan@padjokej.dev>
2026-08-07 11:17:41 +0200

    chore: prepare new sync script

--- syncnotes.sh
+++ syncnotes.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
+rm -r content/ba1-EPFL
+mkdir content/ba1-EPFL
 
-rm -r content/ba2-EPFL
-mkdir content/ba2-EPFL
+cp -v ~/ba1-EPFL/ba1-EPFL/IC-BA1.md content/ba1-EPFL/
 
-cp -v  ~/ba2-EPFL/ba2-EPFL/IC-BA2.md content/ba2-EPFL/
+cp -rv ~/ba1-EPFL/ba1-EPFL/images content/ba1-EPFL/
 
-cp -rv ~/ba2-EPFL/ba2-EPFL/images content/ba2-EPFL/
-
-cp -rv ~/ba2-EPFL/ba2-EPFL/MATH-106 content/ba2-EPFL/
-cp -rv ~/ba2-EPFL/ba2-EPFL/COM-102 content/ba2-EPFL/
-cp -rv ~/ba2-EPFL/ba2-EPFL/ENV-101 content/ba2-EPFL/
-cp -rv ~/ba2-EPFL/ba2-EPFL/GDE-101 content/ba2-EPFL/
-cp -rv ~/ba2-EPFL/ba2-EPFL/CS-108 content/ba2-EPFL/
-cp -rv ~/ba2-EPFL/ba2-EPFL/CS-173 content/ba2-EPFL/
+cp -rv ~/ba1-EPFL/ba1-EPFL/MATH-1{0,1}1 content/ba1-EPFL/
+cp -rv ~/ba1-EPFL/ba1-EPFL/PHYS-101 content/ba1-EPFL/
 
 npx quartz sync --no-pull
