93279da1662cc1641a97f8281eabcfbdda7c3663
PadjokeJ <jonatan@padjokej.dev>
2026-09-08 14:29:16 +0200

    Quartz sync: Sep 8, 2026, 2:29 PM

---
+++ syncnotes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -r content/ba1-EPFL
+mkdir content/ba1-EPFL
+
+cp -v ~/ba1-EPFL/ba1-EPFL/IC-BA1.md content/ba1-EPFL/
+
+cp -rv ~/ba1-EPFL/ba1-EPFL/images content/ba1-EPFL/
+
+cp -rv ~/ba1-EPFL/ba1-EPFL/MATH-1{0,1}1 content/ba1-EPFL/
+cp -rv ~/ba1-EPFL/ba1-EPFL/PHYS-101 content/ba1-EPFL/
+
+npx quartz sync --no-pull
